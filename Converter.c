955406d33af69d5db934062eaf09eae6ac035cae
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-11 07:13:35 +0200

    Converter

---
+++ Converter.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int D;
+	printf("\tWrite the number in Decimals :\t");
+	scanf("%d",&D);
+	printf("\tThe number in Decimals is :%d\n",D);
+	printf("\tThe number in Octal :%o\n",D);
+	printf("\tThe number in Hexadecimals :%x\n",D);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#I'm just doing some exercises will i'm reading The C Programming Language (2nd Edition)
+1- USD to MAD
+2- Converter
