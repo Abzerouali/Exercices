25c9285130162940e07300c71d3ed549c5aa0607
abzerouali <abderrahmanezerouali98@gmail.com>
2023-06-17 16:02:08 +0200

    commit

--- Converter.c
+++ Converter.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-int main(){
+/**
+ * main - entry point
+ * return: 0 (succes)
+ */
+int main(void)
+{
 	int D;
+
 	printf("\tWrite the number in Decimals :\t");
-	scanf("%d",&D);
-	printf("\tThe number in Decimals is :%d\n",D);
-	printf("\tThe number in Octal :%o\n",D);
-	printf("\tThe number in Hexadecimals :%x\n",D);
+	scanf("%d", &D);
+	printf("\tThe number in Decimals is :%d\n", D);
+	printf("\tThe number in Octal :%o\n", D);
+	printf("\tThe number in Hexadecimals :%x\n", D);
+return (0);
 }
