79b8fdd0326f319f0aedc8ff9c7ba270880d8eea
abzerouali <abderrahmanezerouali98@gmail.com>
2023-07-12 20:14:37 +0200

    factoriel.c

---
+++ random/factoriel.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+unsigned int factorial(unsigned int n)
+{
+	if (n == 0)
+		return 1;
+}
+int main()
+{
+	unsigned int num = 5;
+	unsigned int result = factorial(num);
+
+	printf("Factorial of %u is %u\n", num, result);
+	return 0;
+}
