697154986e4e34aa3624e754e3fbfb85913b3cc5
abzerouali <abderrahmanezerouali98@gmail.com>
2023-07-15 10:25:42 +0200

    update

---
+++ random/pointerstring.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	char *message = "Hello, world!"; //Declaring a pointer to a string
+
+	printf("%s\n", message); //Printing the string using the pointer
+	return 0;
+}
